8e3a52c6b481f108eacef1a4aace0d4e3b0a9c01
Abdulwahaab <aarif4@github.com>
2022-05-29 22:36:48 -0400

    cleaned up creating and destruction of handlers

--- alog/alog.py
+++ alog/alog.py
@@ -9,84 +9,109 @@ from termcolor import colored
 
 from .errors import *
 
+timezone_fcn = time.localtime
 
 class logger:
-    __log_levels = ['NOTSET','DEBUG','INFO','WARNING','ERROR','CRITICAL/FATAL']
+    _log_levels = [logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARN, logging.WARNING, logging.ERROR, logging.FATAL, logging.CRITICAL]
 
     _logger : logging.Logger
-    name : str
-    filename : str
-    print_log_lvl : int
-    file_log_level : int
+    _name : str
+    _filename : str
 
     def __init__( 
         self, 
         log_name = 'root', 
         filename = '', 
-        print_log_lvl = logging.INFO, 
-        file_log_level = logging.NOTSET ):
+        level = logging.INFO, 
+        file_level = logging.NOTSET):
 
         if not log_name or not isinstance(log_name, str):
             raise aLogInputArgError(
-                'Invalid name given (got {}). Must be a nonempty string'.format(log_name))
+                'Invalid name given (got "{}"). Must be a nonempty string'.format(log_name))
+        self._name = log_name
 
-        if not isinstance(filename, str) or os.path.isdir(os.path.dirname(filename)):
+        if not filename or not isinstance(filename, str) or os.path.isdir(os.path.dirname(filename)):
             raise aLogInputArgError(
-                'Invalid filename given. Must be a string'
+                'Invalid filename given. Must be a string (can be empty)'
             )
-        if log_name in logging.Logger.manager.loggerDict:
-            self.__logger = logging.getLogger(log_name)
-        # you can find a logger from the dict keys here: logging.root.manager.loggerDict
+        if not filename:
+            self._filename = ''
+        else:
+            self._filename = os.path.abspath(filename)
 
+        if self._name in logging.Logger.manager.loggerDict:
+            self._logger = logging.getLogger(self._name)
+        
+        self._set_log_levels()
+        
+        self._logger = logging.getLogger(self._name)
+        if self._logger.hasHandlers() and len(self._logger.handers) > 2:
+            # this was made by someone else
+            raise aLogUnknownHandlers('Found {} handlers in this already-existing logger'.format(len(self._logger.handers)))
+        
+        self._logger.setLevel(level=logging.NOTSET)
+
+        if level not in self._log_levels:
+            raise aLogInputArgError('Got invalid level for stderr logging. Got ({})'.format(level))
+        self._set_stderr_handler(level)
+
+        if file_level not in self._log_levels:
+            raise aLogInputArgError('Got invalid level for stderr logging. Got ({})'.format(file_level))
+        self._set_file_handler(file_level)
+        
+        self._set_timezone(timezone_fcn)
+    
+    def _set_log_levels(self):
         logging.addLevelName(logging.DEBUG,   colored('DEBUG','cyan'))
         logging.addLevelName(logging.INFO,    colored('INFO ','green'))
         logging.addLevelName(logging.WARNING, colored('WARN ','yellow'))
         logging.addLevelName(logging.ERROR,   colored('ERROR','red'))
         logging.addLevelName(logging.FATAL,   colored('FATAL','magenta'))
 
-        fmt = self.get_format()
-
-        self._logger = logging.getLogger(log_name)
-        self._logger.setLevel(level=logging.NOTSET)
-
-        ch = logging.StreamHandler()
-        ch.setLevel(level=print_log_lvl)
-        ch.setFormatter(fmt)
-        self._logger.addHandler(ch)
-
-        if filename:
-            fp = logging.FileHandler(filename=filename)
-            fp.setLevel(level=file_log_level)
-            fp.setFormatter(fmt)
-            self._logger.addHandler(fp)
-        
-        self.set_timezone(time.localtime)
-
-            #if filename:
-            #    self.fp = logging.FileHandler(filename=filename)
-            #    self.fp.setLevel(level=log_lvl)
-            #    self.fp.setFormatter(fmt)
-            #    self.log.addHandler(self.fp)
-    
-    def get_format(self):
+    def _get_format(self):
         return logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s','%Y-%m-%d %H:%M:%S')
     
-    def set_timezone(self, struct_time_fcn):
+    def _set_timezone(self, struct_time_fcn):
         fmt = self.get_format()
         fmt.converter = struct_time_fcn
         for handler in self._logger.handlers:
             handler.setFormatter(fmt)
     
+    def _set_stderr_handler(self, print_log_lvl):
+        ch = logging.StreamHandler()
+        ch_name = getattr(ch.stream, 'name', '')
+        for handler in self._logger.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                hndl_name = getattr(handler.stream, 'name', '')
+                if hndl_name == ch_name:
+                    # don't make one, it's already there
+                    return
+                else:
+                    raise aLogUnknownHandlers('Found unknown StreamHandler in logger (got {})'.format(hndl_name))
+        
+        ch.setLevel(level=print_log_lvl)
+        ch.setFormatter(self._get_format())
+        self._logger.addHandler(ch)
+
+    def _set_file_handler(self, file_log_level):
+        for handler in self._logger.handlers:
+            if isinstance(handler, logging.FileHandler):
+                hndl_name = getattr(handler.stream, 'name', '')
+                if hndl_name == self._filename:
+                    # don't make one, it's already there
+                    return
+                else:
+                    raise aLogUnknownHandlers('Found FileHandler in logger with an unknown file (got {})'.format(hndl_name))
+        
+        fp = logging.FileHandler(filename=self._filename)
+        fp.setLevel(level=file_log_level)
+        fp.setFormatter(self._get_format())
+        self._logger.addHandler(fp)
+    
     def __del__(self):
-        if not self.USE_PRINTF:
-            if self.log:
-                if self.ch:
-                    self.log.removeHandler(self.ch)
-                    del self.ch
-                if self.fp:
-                    self.log.removeHandler(self.fp)
-                    del self.fp
-                del self.log
+        # just delete the handlers, we'll recreate them the next time we instantiate this logger
+        while self._logger.hasHandlers():
+            self._logger.removeHandler(self._logger.handlers[0])
     
     def debug(self, string):
         self.log.debug(string)
@@ -107,6 +132,14 @@ class logger:
         self.log.exception(string)
         # here it'll already call traceback.print_exception() to get the traceback
     
+    def __repr__(self):
+        return f'alog.{self.__class__.__name__}("{self._name}","{self._filename}")'
+
     def __str__(self):
         # in here you should try .isEnabledFor() to see what levels it will handle
         return ""
+
+__all__ = [
+    "timezone_fcn",
+    "logger"
+]
